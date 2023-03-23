481d0edad1491330feb218300cf8d7e47483f590
ujjawala397 <ujjawala397@gmail.com>
2023-03-23 01:39:10 -0400

    cdpd 14 fixed made dynamic

--- src/pages/counsellor/cdpd14.js
+++ src/pages/counsellor/cdpd14.js
@@ -1,21 +1,23 @@
 import Head from "next/head";
-import { Button,Box,Grid} from "@mui/material";
+import { Button,Box,Grid,TextField} from "@mui/material";
 import { counsellorGetAllAppointmentByDate } from "src/api/Api";
 import { useState, useEffect } from "react";
 import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
 import { AppointmentTableCounsellor } from "src/sections/counsellor/appointment-table-counsellor";
 const Page = () => {
   const [counsellorAppointmentList, setCounsellorAppointmentList] = useState(null);
+  const formattedDateTime = null;
+  const minDateTime = new Date().toISOString().slice(0, -8);
   const [date, setDate] = useState(""); 
   const handleSubmit =  async (event) => {
       //Counsellor Get appointment details by date
-      setDate("2023-10-10");
+      
     }
     useEffect(()=>{
       const fetchAppointments = async () => {
         if(date){
-          // const token = window.sessionStorage.getItem("token");
-          const token= "febab8f0f5e9a0a58d99f844981bc82720eca7b8";
+          const token = window.sessionStorage.getItem("token");
+          // const token= "febab8f0f5e9a0a58d99f844981bc82720eca7b8";
           const res = await counsellorGetAllAppointmentByDate({date,token});
           setCounsellorAppointmentList(res);
           console.log(token)
@@ -24,6 +26,20 @@ const Page = () => {
       fetchAppointments()
     },[date])
     console.log({counsellorAppointmentList})
+
+    const handleDateTimeChange = (event) => {
+      setDate(event.target.value)
+    }
+    useEffect(()=>{
+      const settingDate = async () => {
+        const newDateTime = new Date(date);
+        console.log(date);
+        // setDate(
+        //   {newDateTime.toISOString().substring(0, 10) + "T" + newDateTime.toTimeString().substring(0, 8)}
+        // );
+      }
+      settingDate()
+    },[date])
   return (
     <>
       <Head>
@@ -37,7 +53,22 @@ const Page = () => {
         }}
       >
         <>
-          <Grid item xs={12} style={{ textAlign: "center" }}>
+          <Grid item xs={8} style={{ textAlign: "center" }}>
+            <TextField
+              variant="outlined"
+              required
+              InputLabelProps={{
+                shrink: true,
+                style: { marginTop: "0.2rem" },
+              }}
+              format="yyyy-MM-dd"
+              value={formattedDateTime}
+              inputProps={{ min: minDateTime }}
+              label="Date and Time"
+              type="date"
+              onChange={(event) => handleDateTimeChange(event)}
+            />
+            <br/>
             <Button onClick={handleSubmit}variant="contained" color="primary"> 
             View Appointments
             </Button>

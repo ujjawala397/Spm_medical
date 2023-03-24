5a07b10eb63311ccbf17d03f5836110c71071bd3
ujjawal <ujjawala397@gmail.com>
2023-03-24 17:53:25 -0400

    cdpd 12 - as a doctor i should be able to check patient assessment form

--- src/sections/doctors/doctor-manage-patient-tables.js
+++ src/sections/doctors/doctor-manage-patient-tables.js
@@ -1,18 +1,63 @@
 import PropTypes from "prop-types";
-
+import { useState, useEffect } from "react";
+import { getSelfAssessment } from "src/api/Api";
+import {convertToList} from '../../data/selfassessmentmap';
 import {
   TableCell,
   TableRow,
   Typography,
-  Box
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Dialog,
+  DialogTitle,
+  DialogContent,
 } from "@mui/material";
 import { useRouter } from "next/navigation";
+
 export const DoctorManagePatientTable = (props) => {
-  const {
-    items = null,
-  } = props;
-  
+  const { items = null} = props;
+
+  const styles = {
+    primary: {
+      fontSize: "16px",
+      fontWeight: 600,
+      color: "black",
+    },
+    secondary: {
+      fontSize: "14px",
+      fontWeight: 600,
+      color: "blue",
+    },
+  };
+
+  const [openReport, setOpenReport] = useState(false);
+  const [openAction, setOpenAction] = useState(false);
+  const [listofQuestions, setList] = useState(new Map([]));
   const router = useRouter();
+  
+  
+  const handleClose = async () => {
+    console.log("Here");
+    setOpenReport(false);
+    setOpenAction(false);
+  };
+
+
+  const handlegetAssessment = async (email) => {
+    const token = window.sessionStorage.getItem("token");
+    console.log(email + " Email");
+    const res = await getSelfAssessment({ token, email });
+    if (res) {
+      setOpenReport(true);
+      const list = await convertToList(res);
+      setList(list);
+    }
+    //TODO ERROR
+  };
+
   if(items && !items.length){
     return <div>
     No data found
@@ -37,6 +82,39 @@ export const DoctorManagePatientTable = (props) => {
              <TableCell><Typography variant="h6">{appointment.Firstname} {appointment.Lastname}</Typography></TableCell>
              <TableCell><Typography variant="h6">{appointment.Patient}</Typography></TableCell>
              <TableCell><Typography variant="h6">{appointment.AssignDoctor}</Typography></TableCell>
+             <Button onClick={()=>handlegetAssessment(appointment.Patient)} variant="contained"> View File</Button>
+             <Dialog
+                          BackdropProps={{
+                            invisible: true,
+                            style: { opacity: 1 },
+                          }}
+                          maxWidth="lg"
+                          fullWidth={true}
+                          PaperProps={{
+                            style: {
+                              maxHeight: "90vh",
+                              minHeight: "90vh",
+                            },
+                          }}
+                          open={openReport}
+                          onClose={handleClose}
+                        >
+                          <DialogTitle>Self Assessment Result</DialogTitle>
+                          <DialogContent>
+                            <List>
+                              {Array.from(listofQuestions).map(([question, answer], index) => (
+                                <ListItem key={index}>
+                                  <ListItemText
+                                    primary={question}
+                                    secondary={answer}
+                                    primaryTypographyProps={{ style: styles.primary }}
+                                    secondaryTypographyProps={{ style: styles.secondary }}
+                                  />
+                                </ListItem>
+                              ))}
+                            </List>
+                          </DialogContent>
+             </Dialog>
             
           </TableRow>
             );
